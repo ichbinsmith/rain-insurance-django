1c3b5d2750d47f2861f003df1c71703c3c4ed6d0
Smith Djamoura <smith-etchanm.djamoura@etu.unice.fr>
2020-11-14 00:22:11 +0100

    allowed host config

--- rain_insurance_django/settings.py
+++ rain_insurance_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fpftp6rqg)la+8nz=5qm0^^*!(q$$#58*@_hm-!e5r6vx)$edj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'rainydayshero.herokuapp.com']
 
 
 # Application definition
