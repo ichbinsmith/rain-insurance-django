f2d251473ba24640f47d0aed09361dabfa9eb11d
Smith Djamoura <smith-etchanm.djamoura@etu.unice.fr>
2020-11-14 01:20:09 +0100

    add django_heroku

--- rain_insurance_django/settings.py
+++ rain_insurance_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku #help with staticfiles...
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "RainyDaysHero/static")
-]
+django_heroku.settings(locals())
