9ddc31f5814c5210d324d60de8ef7ceb00be9d4b
Smith Djamoura <smith-etchanm.djamoura@etu.unice.fr>
2021-02-04 15:54:04 +0100

    update GUI

--- RainyDaysHero/app_forms/termInsuranceForm.py
+++ RainyDaysHero/app_forms/termInsuranceForm.py
@@ -36,10 +36,10 @@ class TermInsuranceReserveForm(forms.Form):
     contractOrTotal.widget.attrs.update({'class': 'form-control','onchange':'reserveTypeSwitch()'})
 
     mortalityStress = forms.FloatField(label='Stress on mortality table (%)')
-    mortalityStress.widget.attrs.update({'class': 'form-control','value':0.1})
+    mortalityStress.widget.attrs.update({'class': 'form-control','value':0})
 
     interestRateStress = forms.FloatField(label='Stress on interest rate (%)')
-    interestRateStress.widget.attrs.update({'class': 'form-control','value':0.1})
+    interestRateStress.widget.attrs.update({'class': 'form-control','value':0})
 
     adaptedModel = forms.ChoiceField(label = 'Adapted model',choices = ( ("No", "No"), ("Yes", "Yes") ))
     adaptedModel.widget.attrs.update({'class': 'form-control'})
