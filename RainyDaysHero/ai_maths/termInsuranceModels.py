a5ba3e9cfc520d2295995688f1cc4b3f32670d25
ichbinarnaud <74835076+ichbinarnaud@users.noreply.github.com>
2021-02-05 02:36:57 +0100

    change in the call of stress functions

--- RainyDaysHero/ai_maths/termInsuranceModels.py
+++ RainyDaysHero/ai_maths/termInsuranceModels.py
@@ -34,7 +34,6 @@ def reserves_predicted_scale_knn(x,n,i,a,m,stress_MT=0,stress_interest_rates=0, 
 
 
 
-#Stress test
 
 def lx_evolution(stress):
 	return stresstest.lx_evolution(stress)
@@ -43,41 +42,34 @@ def qx_evolution(stress):
 
 
 
-##plot le calcul actuariel stressé sur la mortalité
 def plot_p_and_l_point(TH,x,i,n,m,a):
 	return stresstest.plot_p_and_l_point(TH,x,i,n,m,a)
 
-##plot le calcul actuariel stressé sur les taux d'intérêts
 def plot_p_and_l_point_interest(TH,x,i,n,m,a):
 	return stresstest.plot_p_and_l_point_interest(TH,x,i,n,m,a)
 
-##plot la prédiction avec le stress sur la mortalité
-def plot_p_and_l_point_knn(TH,x,i,n,m,a,nn=10):
-	return stresstest.plot_p_and_l_point_knn(TH,x,i,n,m,a,nn)
-##	plot la prédiction avec le stress sur les intérêtd
-def plot_p_and_l_point_interest_knn(TH,x,i,n,m,a,nn=10):
-	return stresstest.plot_p_and_l_point_interest_knn(TH,x,i,n,m,a,nn)
+def plot_p_and_l_point_knn(TH,x,i,n,m,a,degree=8):
+	return stresstest.plot_p_and_l_point_knn(TH,x,i,n,m,a,degree)
 
-##Ici si tu mets True ça r$te prédit le modèle adapté sur la table de mortalité et False c'est sur les intérêts
+def plot_p_and_l_point_interest_knn(TH,x,i,n,m,a,degree=8):
+	return stresstest.plot_p_and_l_point_interest_knn(TH,x,i,n,m,a,degree)
+
+##stress_MT =true is for the stress on mortality table and if it's false it's on the interest rates
 def plot_p_and_l_point_new(x,m,n,i,a,stress_MT=True):
 	return stresstest.plot_p_and_l_point_new(x,m,n,i,a,stress_MT)
 
 
 ## Balance sheet#
 
-##Cette fonction renvoie de quoi faire le bilan pour le total en fonction des stress avec le calcul actuariel
 def total_balance_sheet_true(stress_MT=0,stress_interest_rates=0, adapt=True):
 	return balance_sheet.total_balance_sheet_true(stress_MT,stress_interest_rates, adapt)
 
-##Cette fonction renvoie de quoi faire le bilan pour le total en fonction des stress avec l'IA'
 def total_balance_sheet_predicted(stress_MT=0,stress_interest_rates=0, adapt=True):
 	return balance_sheet.total_balance_sheet_predicted(stress_MT,stress_interest_rates, adapt)
 
 
-##Cette fonction renvoie de quoi faire le bilan pour un contrat avec l'IA
 def balance_sheet_knn(x,n,i,a,m,stress_MT=0,stress_interest_rates=0, adapt=True):
 	return balance_sheet.balance_sheet_knn(x,n,i,a,m,stress_MT,stress_interest_rates, adapt)
 
-##Cette fonction renvoie de quoi faire le bilan pour un contrat le calcul actuariel
 def balance_sheet_true(x,n,i,a,m,stress_MT=0,stress_interest_rates=0, adapt=True):
 	return balance_sheet.balance_sheet_true(x,n,i,a,m,stress_MT,stress_interest_rates, adapt)
